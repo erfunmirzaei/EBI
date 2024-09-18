1559f3daf580c199c0b4f139883753497be0822f
Erfan Mirzaei <erfunmirzaei@gmail.com>
2024-09-18 19:02:04 +0200

    Update import statements in simulation.py and transfer_op.py

--- OU_Cov_Est/corr_est_cov_est.py
+++ OU_Cov_Est/corr_est_cov_est.py
@@ -8,21 +8,19 @@ def make_block_matrices(matrix, tau):
     n_blocks = n // tau
     blocks = matrix.reshape(n_blocks, tau, n_blocks, tau)
     block_matrix_result = blocks.transpose(0, 2, 1, 3)
-
+    
     return block_matrix_result
 
-def biased_covariance_estimator(cov_mtx, tau, c_h, b_h):
+def biased_covariance_estimator(cov_mtx, tau):
     n = cov_mtx.shape[0]
     block_cov_matrix = make_block_matrices(cov_mtx,tau) /np.sqrt(n*tau)
-    block_cov_matrix = (block_cov_matrix - b_h) / (c_h - b_h) # Normalize the block covariance matrix
     diag_blocks = torch.diagonal(torch.from_numpy(block_cov_matrix), offset=0, dim1=0, dim2=1)
     return torch.sum(torch.pow(diag_blocks,2))
 
-def unbiased_covariance_estimator(cov_mtx, tau, c_h, b_h):
+def unbiased_covariance_estimator(cov_mtx, tau):
     n = cov_mtx.shape[0]
     m = n / (2*tau)
     block_cov_matrix = make_block_matrices(cov_mtx,tau)/np.sqrt(n*tau)
-    block_cov_matrix =  (block_cov_matrix - b_h) / (c_h - b_h) # Normalize the block covariance matrix
     block_cov_matrix = torch.from_numpy(block_cov_matrix)
 
     diag_blocks = torch.diagonal(block_cov_matrix, offset=0, dim1=0, dim2=1)
@@ -31,9 +29,9 @@ def unbiased_covariance_estimator(cov_mtx, tau, c_h, b_h):
         for i in range(2,n, 2):
             diag_blocks = torch.diagonal(block_cov_matrix, offset=i*signed, dim1=0, dim2=1)
             sum -= torch.sum(torch.pow(diag_blocks,2)) / (m-1)
-
+    
     # print(plus_term, minus_term)
     if sum < 0:
         sum = 0.0
 
-    return sum /(n*tau)
+    return sum 
--- OU_Cov_Est/simulation.py
+++ OU_Cov_Est/simulation.py
@@ -1,6 +1,35 @@
+import importlib
+import subprocess
+import sys
+for module in ['kooplearn', 'datasets', 'matplotlib', 'ml-confs']: # !! Add here any additional module that you need to install on top of kooplearn
+    try:
+        importlib.import_module(module)
+    except ImportError:
+        if module == 'kooplearn':
+            module = 'kooplearn[full]'
+        # pip install -q {module}
+        subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+
 from pathlib import Path
 import random
 import ml_confs
+import matplotlib.pyplot as plt
+import torch
+import json
+from typing import NamedTuple, Tuple
+import numpy as np
+from datetime import datetime
+from tqdm import tqdm
+import pickle
+from  src import OU_process
+from tqdm import tqdm
+from kooplearn.models import Linear, Nonlinear, Kernel
+from kooplearn.models.feature_maps.nn import NNFeatureMap
+from kooplearn.data import traj_to_contexts
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.model_selection import  ParameterGrid
+import time
+CV = True
 
 main_path = Path(__file__).parent
 data_path = main_path / "__data__"
