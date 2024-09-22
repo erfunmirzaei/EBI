bcc54f7e93287780897b53b68ecd8f54bf95b0e2
Erfan Mirzaei <erfunmirzaei@gmail.com>
2024-09-22 22:42:12 +0200

    Refactor configurations and dependencies in OU_Cov_Est and Noisy_Ordered_MNIST modules

--- OU_Cov_Est/result.py
+++ OU_Cov_Est/result.py
@@ -1,42 +1,40 @@
+import importlib
+import subprocess
+import sys
+for module in ['kooplearn', 'datasets', 'matplotlib', 'ml-confs']: # !! Add here any additional module that you need to install on top of kooplearn
+    try:
+        importlib.import_module(module)
+    except ImportError:
+        if module == 'kooplearn':
+            module = 'kooplearn[full]'
+        # pip install -q {module}
+        subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+
+import pathlib as Path
+import ml_confs
 import numpy as np
 import matplotlib.pyplot as plt
 from utils import plot_OU_tau, plot_OU2
 
-n = 10000
-delta = 0.05
+# Load configs
+main_path = Path(__file__).parent
+data_path = main_path / "__data__"
+configs = ml_confs.from_file(main_path / "configs.yaml")
+
 length_scales = [0.05, 0.5, 5]
-pess_bounds = []
-data_bounds_biased = []
-data_bounds_unbiased = []
+Pinelis_bound = []
+Pinelis_emp_bound_biased_cov_est = []
+Pinelis_emp_bound_unbiased_cov_est = []
+M_bound = []
+M_emp_bound_biased_cov_est = []
+M_emp_bound_unbiased_cov_est = []
+taus = []
+
 for l in length_scales:
-    # pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est, taus = Covariance_Estimation(data_points, n, delta, l)
-    # np.save(f'pess_bound_n_{n}_delta_{delta}_l_{l}.npy', pess_bound)
-    # np.save(f'data_bound_biased_cov_est_n_{n}_delta_{delta}_l_{l}.npy', data_bound_biased_cov_est)
-    # np.save(f'data_bound_unbiased_cov_est_n_{n}_delta_{delta}_l_{l}.npy', data_bound_unbiased_cov_est)
-
-    # pess_bounds.append(pess_bound) 
-    # data_bounds_biased.append(data_bound_biased_cov_est)
-    # data_bounds_unbiased.append(data_bound_unbiased_cov_est)
-
-    pess_bounds.append(np.load(f'pess_bound_n_{n}_delta_{delta}_l_{l}.npy'))
-    data_bounds_biased.append(np.load(f'data_bound_biased_cov_est_n_{n}_delta_{delta}_l_{l}.npy'))
-    data_bounds_unbiased.append(np.load(f'data_bound_unbiased_cov_est_n_{n}_delta_{delta}_l_{l}.npy'))
-
-# Create a figure with 3 subplots in a row, single-column width (3.25 inches)
-fig, axes = plt.subplots(1, 3, figsize=(3.25 * 3, 4.5))  # Adjust height as needed for visibility
-
-# Plot each subplot and collect lines for the legend
-lines = []
-for i, length_scale in enumerate(length_scales):
-    show_ylabel = (i == 0)  # Only show y-axis label on the first subplot
-    lines += plot_OU_tau(axes[i], pess_bounds[i], data_bounds_biased[i], data_bounds_unbiased[i], taus, n=n, delta=delta, length_scale=length_scale, show_ylabel=show_ylabel)
-
-# Create a common legend
-labels = ["Emp. bound (unbiased cov. est.)", "Emp. bound (biased cov. est.)", "Pessimistic bound"]
-fig.legend(lines[:3], labels, loc='upper center', fontsize=10, ncol=3, frameon=False)
-
-# Adjust layout
-plt.tight_layout(rect=[0, 0, 1, 0.85])
-plt.subplots_adjust(top=0.85)  # Add more space between title and legend
-plt.savefig(f"OU_Exp_tau_n_{n}_delta_{delta}.pdf", format="pdf", dpi=600)
-plt.show()
+    Pinelis_bound = np.load(f'Pinelis_bound_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    Pinelis_emp_bound_biased_cov_est = np.load(f'Pinelis_emp_bound_biased_cov_est_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    Pinelis_emp_bound_unbiased_cov_est = np.load(f'Pinelis_emp_bound_unbiased_cov_est_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    M_bound = np.load(f'M_bound_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    M_emp_bound_biased_cov_est = np.load(f'M_emp_bound_biased_cov_est_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    M_emp_bound_unbiased_cov_est = np.load(f'M_emp_bound_unbiased_cov_est_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy')
+    taus = np.load(f'taus_n_{configs.n_plot_tau}_delta_{configs.delta}_l_{l}.npy') 
--- OU_Cov_Est/utils.py
+++ OU_Cov_Est/utils.py
@@ -36,28 +36,48 @@ def get_divisors(n):
     for prime_power_combo in itertools.product(*powers):
         yield prod(prime_power_combo)
 
-def plot_OU_tau(ax, pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est, taus, n, delta, length_scale, show_ylabel=False):
-    pess_bound_mean = np.mean(pess_bound, axis=-1)
-    pess_bound_std = np.std(pess_bound, axis=-1)
+def plot_OU_tau_lenght_scale(ax, Pinelis_bound, Pinelis_emp_bound_biased_cov_est, Pinelis_emp_bound_unbiased_cov_est, M_bound, M_emp_bound_biased_cov_est, M_emp_bound_unbiased_cov_est, taus, length_scale, show_ylabel=False):
+    Pinelis_bound_mean = np.mean(Pinelis_bound, axis=-1)
+    Pinelis_bound_std = np.std(Pinelis_bound, axis=-1)
+    Pinelis_emp_bound_biased_cov_est_mean = np.mean(Pinelis_emp_bound_biased_cov_est, axis=-1)
+    Pinelis_emp_bound_biased_cov_est_std = np.std(Pinelis_emp_bound_biased_cov_est, axis=-1)
+    Pinelis_emp_bound_unbiased_cov_est_mean = np.mean(Pinelis_emp_bound_unbiased_cov_est, axis=-1)
+    Pinelis_emp_bound_unbiased_cov_est_std = np.std(Pinelis_emp_bound_unbiased_cov_est, axis=-1)
 
-    data_bound_biased_est_mean = np.mean(data_bound_biased_cov_est, axis=-1)
-    data_bound_biased_est_std = np.std(data_bound_biased_cov_est, axis=-1)
-
-    data_bound_unbiased_est_mean = np.mean(data_bound_unbiased_cov_est, axis=-1)
-    data_bound_unbiased_est_std = np.std(data_bound_unbiased_cov_est, axis=-1)
+    M_bound_mean = np.mean(M_bound, axis=-1)
+    M_bound_std = np.std(M_bound, axis=-1)
+    M_emp_bound_biased_cov_est_mean = np.mean(M_emp_bound_biased_cov_est, axis=-1)
+    M_emp_bound_biased_cov_est_std = np.std(M_emp_bound_biased_cov_est, axis=-1)    
+    M_emp_bound_unbiased_cov_est_mean = np.mean(M_emp_bound_unbiased_cov_est, axis=-1)
+    M_emp_bound_unbiased_cov_est_std = np.std(M_emp_bound_unbiased_cov_est, axis=-1)
 
     # Plot with appropriate figure size and font sizes
-    line1, = ax.loglog(taus, data_bound_unbiased_est_mean, marker='o', label="Emp. bound (unbiased cov. est.)", linewidth=1)
-    ax.fill_between(taus, data_bound_unbiased_est_mean - data_bound_unbiased_est_std, 
-                    data_bound_unbiased_est_mean + data_bound_unbiased_est_std, alpha=0.2)
+    line1, = ax.loglog(taus, Pinelis_emp_bound_unbiased_cov_est_mean, marker='o', label="Pinelis bound", linewidth=1)
+    ax.fill_between(taus, Pinelis_emp_bound_unbiased_cov_est_mean - Pinelis_emp_bound_unbiased_cov_est_std,
+                    Pinelis_emp_bound_unbiased_cov_est_mean + Pinelis_emp_bound_unbiased_cov_est_std, alpha=0.2)
 
-    line2, = ax.loglog(taus, data_bound_biased_est_mean, marker='s', label="Emp. bound (biased cov. est.)", linewidth=1)
-    ax.fill_between(taus, data_bound_biased_est_mean - data_bound_biased_est_std, 
-                    data_bound_biased_est_mean + data_bound_biased_est_std, alpha=0.2)
+    line2, = ax.loglog(taus, Pinelis_emp_bound_biased_cov_est_mean, marker='s', label="Pinelis emp. bound (biased cov. est.)", linewidth=1)
+    ax.fill_between(taus, Pinelis_emp_bound_biased_cov_est_mean - Pinelis_emp_bound_biased_cov_est_std,
+                    Pinelis_emp_bound_biased_cov_est_mean + Pinelis_emp_bound_biased_cov_est_std, alpha=0.2)
+    
 
-    line3, = ax.loglog(taus, pess_bound_mean, marker='^', label="Pessimistic bound", linewidth=1)
-    ax.fill_between(taus, pess_bound_mean - pess_bound_std, 
-                    pess_bound_mean + pess_bound_std, alpha=0.2)
+    line3, = ax.loglog(taus, Pinelis_emp_bound_unbiased_cov_est_mean, marker='s', label="Pinelis emp. bound (unbiased cov. est.)", linewidth=1)
+    ax.fill_between(taus, Pinelis_emp_bound_unbiased_cov_est_mean - Pinelis_emp_bound_unbiased_cov_est_std,
+                    Pinelis_emp_bound_unbiased_cov_est_mean + Pinelis_emp_bound_unbiased_cov_est_std, alpha=0.2)
+    
+    
+    line4 = ax.loglog(taus, M_bound_mean, marker='^', label="M bound", linewidth=1)
+    ax.fill_between(taus, M_bound_mean - M_bound_std,
+                    M_bound_mean + M_bound_std, alpha=0.2)
+        
+    line5 = ax.loglog(taus, M_emp_bound_biased_cov_est_mean, marker='P', label="M emp. bound (biased cov. est.)", linewidth=1)
+    ax.fill_between(taus, M_emp_bound_biased_cov_est_mean - M_emp_bound_biased_cov_est_std,
+                    M_emp_bound_biased_cov_est_mean + M_emp_bound_biased_cov_est_std, alpha=0.2)
+    
+    line6 = ax.loglog(taus, M_emp_bound_unbiased_cov_est_mean, marker='P', label="M emp. bound (unbiased cov. est.)", linewidth=1)
+    ax.fill_between(taus, M_emp_bound_unbiased_cov_est_mean - M_emp_bound_unbiased_cov_est_std,
+                    M_emp_bound_unbiased_cov_est_mean + M_emp_bound_unbiased_cov_est_std, alpha=0.2)
+    
 
     ax.set_xlabel("Block size", fontsize=10)
     if show_ylabel:
@@ -66,87 +86,110 @@ def plot_OU_tau(ax, pess_bound, data_bound_biased_cov_est, data_bound_unbiased_c
     ax.tick_params(axis='both', which='major', labelsize=8)
     ax.grid(True)
     
-    return line1, line2, line3
+    return line1, line2, line3, line4, line5, line6
 
-def plot_OU2(pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est,True_value, Ns, delta, length_scale):
-    pess_bound_mean = np.mean(pess_bound, axis=-1)
-    pess_bound_std = np.std(pess_bound, axis=-1)
+def plot_OU_tau(configs, length_scales, Pinelis_bound, Pinelis_emp_bound_biased_cov_est, Pinelis_emp_bound_unbiased_cov_est, M_bound, M_emp_bound_biased_cov_est, M_emp_bound_unbiased_cov_est, taus, length_scale):
+    # Create a figure with 3 subplots in a row, single-column width (3.25 inches)
+    fig, axes = plt.subplots(1, len(length_scales), figsize=(3.25 * 3, 4.5))  # Adjust height as needed for visibility
 
-    data_bound_biased_est_mean = np.mean(data_bound_biased_cov_est, axis=-1)
-    data_bound_biased_est_std = np.std(data_bound_biased_cov_est, axis=-1)
+    # Plot each subplot and collect lines for the legend
+    lines = []
+    for i, length_scale in enumerate(length_scales):
+        show_ylabel = (i == 0)  # Only show y-axis label on the first subplot
+        lines += plot_OU_tau(axes[i],Pinelis_bound, Pinelis_emp_bound_biased_cov_est, Pinelis_emp_bound_unbiased_cov_est, M_bound, M_emp_bound_biased_cov_est, M_emp_bound_unbiased_cov_est, taus, length_scale, show_ylabel=show_ylabel)
 
-    data_bound_unbiased_est_mean = np.mean(data_bound_unbiased_cov_est, axis=-1)
-    data_bound_unbiased_est_std = np.std(data_bound_unbiased_cov_est, axis=-1)
+    # Create a common legend
+    labels = ["Pinelis bound", "Pinelis emp. bound (biased cov. est.)", "Pinelis emp. bound (unbiased cov. est.)", "M bound", "M emp. bound (biased cov. est.)", "M emp. bound (unbiased cov. est.)"]
+    n_labels = len(labels)
+    fig.legend(lines[:n_labels], labels, loc='upper center', fontsize=10, ncol=len(length_scales), frameon=False)
 
-    true_value_mean = np.mean(True_value, axis=-1)
-    true_value_std = np.std(True_value, axis=-1)
-
-    # Plot with larger figure size and font sizes
-    plt.figure(figsize=(12, 8))  # Adjust figure size as needed
-
-    plt.loglog(Ns, data_bound_unbiased_est_mean, marker='o', label="Emp. bound (unbiased cov. est.)", linewidth=2)
-    plt.fill_between(Ns, data_bound_unbiased_est_mean - data_bound_unbiased_est_std, 
-                     data_bound_unbiased_est_mean + data_bound_unbiased_est_std, alpha=0.2)
-
-    plt.loglog(Ns, data_bound_biased_est_mean, marker='s', label="Emp. bound (biased cov. est.)", linewidth=2)
-    plt.fill_between(Ns, data_bound_biased_est_mean - data_bound_biased_est_std, 
-                     data_bound_biased_est_mean + data_bound_biased_est_std, alpha=0.2)
-
-    plt.loglog(Ns, pess_bound_mean, marker='^', label="Pessimistic bound", linewidth=2)
-    plt.fill_between(Ns, pess_bound_mean - pess_bound_std, 
-                     pess_bound_mean + pess_bound_std, alpha=0.2)
-    
-    plt.loglog(Ns, true_value_mean, marker='P', label= "Estimated True value", linewidth=2)
-    plt.fill_between(Ns, true_value_mean - true_value_std, 
-                     true_value_mean + true_value_std, alpha=0.2)
-
-    plt.xlabel("Number of training samples", fontsize=18)
-    plt.ylabel("Covariance upper bound", fontsize=18)
-    # plt.title(f"Covariance Upper Bounds (delta = {delta}) of OU Process with Gaussian Kernel (l={length_scale})", fontsize=16)
-
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-    plt.legend(fontsize=16)
-    plt.grid(True)
-    
-    plt.savefig(f"OU_Exp_delta_{delta}_l_{length_scale}.pdf", format="pdf", dpi=900)
+    # Adjust layout
+    plt.tight_layout(rect=[0, 0, 1, 0.85])
+    plt.subplots_adjust(top=0.85)  # Add more space between title and legend
+    plt.savefig(f"OU_Exp_tau_n_{configs.n_plot_tau}_delta_{configs.delta}.pdf", format="pdf", dpi=600)
     plt.show()
 
-def plot_OU2(ax, pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est, True_value, Ns, delta, length_scale, show_ylabel=False):
-    pess_bound_mean = np.mean(pess_bound, axis=-1)
-    pess_bound_std = np.std(pess_bound, axis=-1)
 
-    data_bound_biased_est_mean = np.mean(data_bound_biased_cov_est, axis=-1)
-    data_bound_biased_est_std = np.std(data_bound_biased_cov_est, axis=-1)
 
-    data_bound_unbiased_est_mean = np.mean(data_bound_unbiased_cov_est, axis=-1)
-    data_bound_unbiased_est_std = np.std(data_bound_unbiased_cov_est, axis=-1)
+# def plot_OU2(pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est,True_value, Ns, delta, length_scale):
+#     pess_bound_mean = np.mean(pess_bound, axis=-1)
+#     pess_bound_std = np.std(pess_bound, axis=-1)
 
-    true_value_mean = np.mean(True_value, axis=-1)
-    true_value_std = np.std(True_value, axis=-1)
+#     data_bound_biased_est_mean = np.mean(data_bound_biased_cov_est, axis=-1)
+#     data_bound_biased_est_std = np.std(data_bound_biased_cov_est, axis=-1)
 
-    # Plot with appropriate figure size and font sizes
-    line1, = ax.loglog(Ns, data_bound_unbiased_est_mean, marker='o', label="Emp. bound (unbiased cov. est.)", linewidth=1)
-    ax.fill_between(Ns, data_bound_unbiased_est_mean - data_bound_unbiased_est_std, 
-                    data_bound_unbiased_est_mean + data_bound_unbiased_est_std, alpha=0.2)
+#     data_bound_unbiased_est_mean = np.mean(data_bound_unbiased_cov_est, axis=-1)
+#     data_bound_unbiased_est_std = np.std(data_bound_unbiased_cov_est, axis=-1)
 
-    line2, = ax.loglog(Ns, data_bound_biased_est_mean, marker='s', label="Emp. bound (biased cov. est.)", linewidth=1)
-    ax.fill_between(Ns, data_bound_biased_est_mean - data_bound_biased_est_std, 
-                    data_bound_biased_est_mean + data_bound_biased_est_std, alpha=0.2)
+#     true_value_mean = np.mean(True_value, axis=-1)
+#     true_value_std = np.std(True_value, axis=-1)
 
-    line3, = ax.loglog(Ns, pess_bound_mean, marker='^', label="Pessimistic bound", linewidth=1)
-    ax.fill_between(Ns, pess_bound_mean - pess_bound_std, 
-                    pess_bound_mean + pess_bound_std, alpha=0.2)
+#     # Plot with larger figure size and font sizes
+#     plt.figure(figsize=(12, 8))  # Adjust figure size as needed
+
+#     plt.loglog(Ns, data_bound_unbiased_est_mean, marker='o', label="Emp. bound (unbiased cov. est.)", linewidth=2)
+#     plt.fill_between(Ns, data_bound_unbiased_est_mean - data_bound_unbiased_est_std, 
+#                      data_bound_unbiased_est_mean + data_bound_unbiased_est_std, alpha=0.2)
+
+#     plt.loglog(Ns, data_bound_biased_est_mean, marker='s', label="Emp. bound (biased cov. est.)", linewidth=2)
+#     plt.fill_between(Ns, data_bound_biased_est_mean - data_bound_biased_est_std, 
+#                      data_bound_biased_est_mean + data_bound_biased_est_std, alpha=0.2)
+
+#     plt.loglog(Ns, pess_bound_mean, marker='^', label="Pessimistic bound", linewidth=2)
+#     plt.fill_between(Ns, pess_bound_mean - pess_bound_std, 
+#                      pess_bound_mean + pess_bound_std, alpha=0.2)
     
-    line4, = ax.loglog(Ns, true_value_mean, marker='P', label="Estimated True value", linewidth=1)
-    ax.fill_between(Ns, true_value_mean - true_value_std, 
-                    true_value_mean + true_value_std, alpha=0.2)
+#     plt.loglog(Ns, true_value_mean, marker='P', label= "Estimated True value", linewidth=2)
+#     plt.fill_between(Ns, true_value_mean - true_value_std, 
+#                      true_value_mean + true_value_std, alpha=0.2)
 
-    ax.set_xlabel("Number of training samples", fontsize=10)
-    if show_ylabel:
-        ax.set_ylabel("Covariance upper bound", fontsize=10)
-    ax.set_title(f"length scale={length_scale}", fontsize=10)
-    ax.tick_params(axis='both', which='major', labelsize=10)
-    ax.grid(True)
+#     plt.xlabel("Number of training samples", fontsize=18)
+#     plt.ylabel("Covariance upper bound", fontsize=18)
+#     # plt.title(f"Covariance Upper Bounds (delta = {delta}) of OU Process with Gaussian Kernel (l={length_scale})", fontsize=16)
+
+#     plt.xticks(fontsize=16)
+#     plt.yticks(fontsize=16)
+#     plt.legend(fontsize=16)
+#     plt.grid(True)
     
-    return line1, line2, line3, line4
+#     plt.savefig(f"OU_Exp_delta_{delta}_l_{length_scale}.pdf", format="pdf", dpi=900)
+#     plt.show()
+
+# def plot_OU2(ax, pess_bound, data_bound_biased_cov_est, data_bound_unbiased_cov_est, True_value, Ns, delta, length_scale, show_ylabel=False):
+#     pess_bound_mean = np.mean(pess_bound, axis=-1)
+#     pess_bound_std = np.std(pess_bound, axis=-1)
+
+#     data_bound_biased_est_mean = np.mean(data_bound_biased_cov_est, axis=-1)
+#     data_bound_biased_est_std = np.std(data_bound_biased_cov_est, axis=-1)
+
+#     data_bound_unbiased_est_mean = np.mean(data_bound_unbiased_cov_est, axis=-1)
+#     data_bound_unbiased_est_std = np.std(data_bound_unbiased_cov_est, axis=-1)
+
+#     true_value_mean = np.mean(True_value, axis=-1)
+#     true_value_std = np.std(True_value, axis=-1)
+
+#     # Plot with appropriate figure size and font sizes
+#     line1, = ax.loglog(Ns, data_bound_unbiased_est_mean, marker='o', label="Emp. bound (unbiased cov. est.)", linewidth=1)
+#     ax.fill_between(Ns, data_bound_unbiased_est_mean - data_bound_unbiased_est_std, 
+#                     data_bound_unbiased_est_mean + data_bound_unbiased_est_std, alpha=0.2)
+
+#     line2, = ax.loglog(Ns, data_bound_biased_est_mean, marker='s', label="Emp. bound (biased cov. est.)", linewidth=1)
+#     ax.fill_between(Ns, data_bound_biased_est_mean - data_bound_biased_est_std, 
+#                     data_bound_biased_est_mean + data_bound_biased_est_std, alpha=0.2)
+
+#     line3, = ax.loglog(Ns, pess_bound_mean, marker='^', label="Pessimistic bound", linewidth=1)
+#     ax.fill_between(Ns, pess_bound_mean - pess_bound_std, 
+#                     pess_bound_mean + pess_bound_std, alpha=0.2)
+    
+#     line4, = ax.loglog(Ns, true_value_mean, marker='P', label="Estimated True value", linewidth=1)
+#     ax.fill_between(Ns, true_value_mean - true_value_std, 
+#                     true_value_mean + true_value_std, alpha=0.2)
+
+#     ax.set_xlabel("Number of training samples", fontsize=10)
+#     if show_ylabel:
+#         ax.set_ylabel("Covariance upper bound", fontsize=10)
+#     ax.set_title(f"length scale={length_scale}", fontsize=10)
+#     ax.tick_params(axis='both', which='major', labelsize=10)
+#     ax.grid(True)
+    
+#     return line1, line2, line3, line4
